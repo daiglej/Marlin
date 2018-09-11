b9dcfb33845e1564db68409ebcc1e2bbc3417f83
Jonathan Daigle <daigle_jonathan@yahoo.ca>
2018-09-10 21:52:05 -0400

    fix mistake where maximu  speed of z axes was increased instead of extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -618,7 +618,7 @@
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 400, 300, 50, 25 }
+#define DEFAULT_MAX_FEEDRATE          { 400, 300, 25, 50 }
 
 /**
  * Default Max Acceleration (change/s) change = mm/s
